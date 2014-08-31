beb6eecfae564550f965ffb1ccdc42d8723815a4
Stefano Zanella <stefano.zanella@sponsorpay.com>
2014-08-31 13:23:44 +0200

    Create cookbook skeleton.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'znc'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures znc'
+long_description 'Installs/Configures znc'
+version          '0.1.0'
+
